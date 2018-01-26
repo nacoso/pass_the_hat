07294408de1acec8d113dbd072c19a1c6a4df8f8
Nick Cardoso <nac.doso@gmail.com>
2018-01-26 16:53:05 -0500

    fix user model migrations and migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  SUBSCRIPTION_PLANS = [[10, "junior"] ]
 end
+
+
+# User::SUBSCRIPTION_PLANS
--- db/migrate/20180126213438_create_user_charities.rb
+++ db/migrate/20180126213438_create_user_charities.rb
@@ -1,6 +1,9 @@
 class CreateUserCharities < ActiveRecord::Migration[5.1]
   def change
     create_table :user_charities do |t|
+      t.belongs_to :user, foreign_key: true
+      t.belongs_to :charity, foreign_key: true
+      t.string :status
 
       t.timestamps
     end
