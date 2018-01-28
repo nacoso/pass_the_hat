56c4452d89e4edc79f4be89d4cc659fb53648e3c
Alex Clivio <amrclivio@gmail.com>
2018-01-28 14:51:21 -0500

    created stripe checkout

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,13 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+
   root 'home#index'
-=======
 
-
-
-  root "home#index"
-
-
->>>>>>> 439031bac18cbfa8725d0f6c9d13e74a05dfed8b
 
   resources :charities
   resources :donations
   resources :user_charities
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :charges
 end
