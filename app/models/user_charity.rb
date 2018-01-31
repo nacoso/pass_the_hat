30ac20ee792c5a955e564f83694f163b16edc8f5
Nick Cardoso <nac.doso@gmail.com>
2018-01-31 02:02:25 -0500

    added devise :omniauthable to user charity model

--- app/models/user_charity.rb
+++ app/models/user_charity.rb
@@ -2,6 +2,7 @@ class UserCharity < ApplicationRecord
   has_many :donations
   belongs_to :user
   belongs_to :charity
+  devise :omniauthable
 
   before_commit :authenticate_user_charity!
   #further configuration for devise?
