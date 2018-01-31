d19da49d362e1dc7754cf3e7ff5dd9bf70a1b8bb
Nick Cardoso <nac.doso@gmail.com>
2018-01-31 01:21:22 -0500

    ran rails g devise:install. added authentication before action to user charity model

--- app/models/user_charity.rb
+++ app/models/user_charity.rb
@@ -2,4 +2,8 @@ class UserCharity < ApplicationRecord
   has_many :donations
   belongs_to :user
   belongs_to :charity
+
+  before_action :authenticate_user_charity!
+  #further configuration for devise?
+
 end
