8dab57c36154ef608a57c0359c18c2b37edd72f9
Nick Cardoso <nac.doso@gmail.com>
2018-01-31 16:57:03 -0500

    created migration to add email column in user model

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -5,6 +5,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       # The code below is to show you where to access the data.
       # request.env['omniauth.params'].
       # raise request.env["omniauth.auth"].to_yaml
+      
       # Make sure to go to Stripe's Account Settings > Applications and set your Redirect URL to: https://pass-the-hat.herokuapp.com/user_charities/auth/stripe_connect/callback
       # The Webhook URL will be something similar: http://www.yourdomain.com/users/auth/stripe_connect/callback
       # Then you can hit /auth/stripe_connect
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180126215136) do
+ActiveRecord::Schema.define(version: 20180131211527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20180126215136) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
   add_foreign_key "donations", "user_charities"
