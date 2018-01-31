2edd659b9752a4b5b1747209148f64524624644b
Nick Cardoso <nac.doso@gmail.com>
2018-01-31 10:10:39 -0500

    removed omniautho callback code from user_charity model. Only needed in charity model.

--- app/models/user_charity.rb
+++ app/models/user_charity.rb
@@ -2,9 +2,6 @@ class UserCharity < ApplicationRecord
   has_many :donations
   belongs_to :user
   belongs_to :charity
-  devise :omniauthable
-
-  before_commit :authenticate_user_charity!
-  #further configuration for devise?
+  
 
 end
