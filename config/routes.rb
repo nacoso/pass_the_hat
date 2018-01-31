72382febc31c46b2fff1af29c5c6ad2e872eb8c6
Nick Cardoso <nac.doso@gmail.com>
2018-01-31 01:39:29 -0500

    added devise routes for user_charities

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
 
+  devise_for :user_charities, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
+
   root 'home#index'
 
   get 'donations/recurring' => "donations#new_recurring", as: "new_recurring"
   get 'charities/new-' => "charities#form_recurring", as: "form_recurring"
+
   resources :charities
   resources :donations
   resources :user_charities
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   resources :charges
 end
