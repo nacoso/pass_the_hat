d3b413758b0cc0ae415bbd7317abb96c1d945877
Donato Manzione <donato.manzione90@gmail.com>
2018-02-01 08:52:12 -0500

    halfway through figuring out how to allow user login; not sure why it isn't working yet

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root 'home#index'
-  get 'signup'  => 'users#new' 
+  get 'signup'  => 'users#new'
   get 'charities/welcome' => "charities#welcome"
   get 'donations/recurring' => "donations#new_recurring", as: "new_recurring"
   get 'charities/new-' => "charities#form_recurring", as: "form_recurring"
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   resources :user_charities
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+ post 'login' => 'sessions#create'
   resources :charges
+
+  delete 'logout' => 'sessions#destroy'
 end
