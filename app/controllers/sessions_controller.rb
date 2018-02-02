03b66188debd8ac57d8f33d43caace11a2b1fd2a
Donato Manzione <donato.manzione90@gmail.com>
2018-02-02 14:14:54 -0500

    login/logout buttons

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       # Save the user id inside the browser cookie. This is how we keep the user logged in when they navigate around our website.
       session["#{user.class.to_s.downcase}_id".to_sym] = user.id
-      redirect_to '/'
+      redirect_to user
     else
     # If user's login doesn't work, send them back to the login form.
       redirect_to '/login'
@@ -18,7 +18,7 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     session[:charity_id] = nil
-    redirect_to '/login'
+    redirect_to '/'
   end
 
 end
